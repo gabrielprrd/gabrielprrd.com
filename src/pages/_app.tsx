9f81fb877810dce9a263f6feb7b62e965e867c33
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-06-25 20:16:15 +0100

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { type AppType } from "next/dist/shared/lib/utils";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Gabriel Afonso - Frontend Dev</title>
+        <meta name="description" content="Portfolio website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </>
+  );
+}
