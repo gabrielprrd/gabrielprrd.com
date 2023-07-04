7f274e6877b7875d15d9a98b498eabf807732f6f
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-07-04 16:38:34 +0100

    feat: add layout support for typescript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "@/styles/theme";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MyApp: AppType = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />;
+    <ChakraProvider theme={theme}>
+      {getLayout(<Component {...pageProps} />)}
     </ChakraProvider>
   );
 };
