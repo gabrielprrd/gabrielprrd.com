1e22f0e50113675a0f68c10b9427155e9ba67372
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-07-04 16:36:22 +0100

    feat: add _document file

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import theme from "@/styles/theme";
+import { ColorModeScript } from "@chakra-ui/react";
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
