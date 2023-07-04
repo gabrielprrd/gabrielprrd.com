a0b2c3d1acabcdb38b85c1eeb8af2c3a552c97df
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-07-04 16:39:03 +0100

    feat: create homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,15 @@
+import DefaultLayout from "@/components/layouts/DefaultLayout";
+import CompaniesSection from "@/pageComponents/Home/CompaniesSection";
+import HeroSection from "@/pageComponents/Home/HeroSection";
+import ProjectsSection from "@/pageComponents/Home/ProjectsSection";
+import { Divider } from "@chakra-ui/react";
 import Head from "next/head";
+import { chakra } from "@chakra-ui/react";
+import type { ReactElement } from "react";
+
+Home.getLayout = function (page: ReactElement) {
+  return <DefaultLayout>{page}</DefaultLayout>;
+};
 
 export default function Home() {
   return (
@@ -8,7 +19,25 @@ export default function Home() {
         <meta name="description" content="Portfolio website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <chakra.main
+        display="flex"
+        flexDirection="column"
+        gap={{ base: 20, md: 36 }}
+        pb={{ base: 20, md: 36 }}
+      >
+        <section id="hero-section">
+          <HeroSection />
+        </section>
+        <Divider />
+        <section id="companies-section">
+          <CompaniesSection />
+        </section>
+        <Divider />
+        <section id="projects-section">
+          <ProjectsSection />
+        </section>
+        <Divider />
+      </chakra.main>
     </>
   );
 }
