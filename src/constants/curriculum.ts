15a25ab56f73755befb8c6ba45c22ec6e9b4a930
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-07-06 22:01:02 +0100

    docs: update downloadable resume pdf

--- src/constants/curriculum.ts
+++ src/constants/curriculum.ts
@@ -1,2 +1,2 @@
 export const CURRICULUM_PDF =
-  "https://drive.google.com/uc?export=download&id=1fPsgmtqntVX2no93-a4TN6aWLeSSCGTa";
+  "https://drive.google.com/uc?export=download&id=1yUQPo4Pg5zusfMGbVJdVq4X1Ka_zyYqL";
