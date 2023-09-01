b6fbbb9a49358687033300d8c5b0adf06217fe7e
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-09-01 14:36:37 +0200

    feat: add missing aria-label

--- src/components/ThemeSwitcher/index.tsx
+++ src/components/ThemeSwitcher/index.tsx
@@ -4,5 +4,12 @@ export default function ThemeSwitcher(props: SwitchProps) {
   const { toggleColorMode, colorMode } = useColorMode();
   const isDark = colorMode === "light";
 
-  return <Switch onChange={toggleColorMode} isChecked={isDark} {...props} />;
+  return (
+    <Switch
+      onChange={toggleColorMode}
+      isChecked={isDark}
+      aria-label="Toggle theme"
+      {...props}
+    />
+  );
 }
