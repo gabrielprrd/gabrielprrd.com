1fb03504ddb68707e25ad2ac352d54e5f0f56f54
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-09-05 12:33:57 +0200

    refactor: simplify logo

--- src/components/layouts/LogoHomePageLink/index.tsx
+++ src/components/layouts/LogoHomePageLink/index.tsx
@@ -1,18 +1,16 @@
 import Link from "@/components/infra/Link";
-import { Text } from "@chakra-ui/react";
 
 export default function LogoHomePageLink() {
   return (
     <Link
       aria-label="Go to home page"
       href="/"
-      borderBottomColor="green"
-      _hover={{ borderBottomWidth: 2 }}
-      style={{ textDecoration: "none" }}
+      fontSize="2xl"
+      fontWeight="bold"
+      textDecorationLine={"2px"}
+      _hover={{ textDecoration: "underline", textDecorationColor: "green" }}
     >
-      <Text fontSize="2xl" fontWeight="bold" textDecoration="none">
-        gabrielprrd
-      </Text>
+      gabrielprrd
     </Link>
   );
 }
