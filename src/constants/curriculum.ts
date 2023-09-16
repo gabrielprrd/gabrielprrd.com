a256dc5352dbcb7eaf5ed859d87f880a437bedc5
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-09-16 10:24:58 +0200

    feat: update resume file

--- src/constants/curriculum.ts
+++ src/constants/curriculum.ts
@@ -1,3 +1,3 @@
-const FILE_ID = "1xGkTcNmh5MiWyc5Rx9ZBkJApEx-KlQiK";
+const FILE_ID = "1fwMXs_k1TE-CV1AvzkXqcHt-B_h2TkcG";
 
 export const CURRICULUM_PDF = `https://drive.google.com/uc?export=download&id=${FILE_ID}`;
