8e3d85baa23e2e3b2c1a5ea4117808938c3f35cf
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2026-01-17 10:50:25 +0100

    Update resumé link

--- src/constants/curriculum.ts
+++ src/constants/curriculum.ts
@@ -1,3 +1,3 @@
-const FILE_ID = "1fwMXs_k1TE-CV1AvzkXqcHt-B_h2TkcG";
+const FILE_ID = "1YZn4A0MZkMbLbkGCQrMd8jhgvnxyWAWm";
 
 export const CURRICULUM_PDF = `https://drive.google.com/uc?export=download&id=${FILE_ID}`;
